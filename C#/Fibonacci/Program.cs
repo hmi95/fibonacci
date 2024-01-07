6f7425a1623928697bc54852d060ff2ec688fd6d
Harald Ichters <harald@ichters.de>
2024-01-07 15:31:19 +0100

    Added code for C#

---
+++ C#/Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Fibonacci-Folge:");
+
+int secondLast = 1;
+int last = 1;
+
+Console.WriteLine("{0,9:N0}", secondLast);
+Console.WriteLine("{0,9:N0}", last);
+
+int current = secondLast + last;
+
+Console.WriteLine("{0,9:N0}", current);
+
+Console.ReadLine();
